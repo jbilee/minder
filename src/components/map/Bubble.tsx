2891e8abb52200879c12fccd0aa86b98c7196a1c
jbilee <julishahn@gmail.com>
2024-07-25 23:18:13 +0900

    feat: Mind map bubble component

---
+++ src/components/map/Bubble.tsx
@@ -0,0 +1,40 @@
+import { Group, Rect, Text } from "react-konva";
+
+type BubblePropp = {
+  text: string;
+  x: number;
+  y: number;
+};
+
+export default function Bubble({ text, x, y }: BubblePropp) {
+  return (
+    <Group draggable>
+      <Rect
+        cornerRadius={10}
+        x={x}
+        y={y}
+        width={100}
+        height={100}
+        fill="blue"
+      />
+      <Rect
+        cornerRadius={10}
+        x={x - 3}
+        y={y - 3}
+        width={106}
+        height={106}
+        name="barrier"
+      />
+      <Text
+        text={text}
+        fill="white"
+        x={x}
+        y={y}
+        width={100}
+        height={100}
+        align="center"
+        padding={5}
+      />
+    </Group>
+  );
+}
