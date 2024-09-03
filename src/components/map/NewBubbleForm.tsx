74126de0a8057f2a43c537a3724d1c68f5e309dd
jbilee <julishahn@gmail.com>
2024-09-03 19:33:52 +0900

    feat: Add character limit to new bubble form

--- src/components/map/NewBubbleForm.tsx
+++ src/components/map/NewBubbleForm.tsx
@@ -1,25 +1,31 @@
 "use client";
 
-import { type FormEvent, useState } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 
 type NewBubbleFormProps = {
-  createBubble: (text: string) => void;
+  addBubble: (text: string) => void;
 };
 
-export default function NewBubbleForm({ createBubble }: NewBubbleFormProps) {
+export default function NewBubbleForm({ addBubble }: NewBubbleFormProps) {
   const [input, setInput] = useState("");
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    if (input.length > 30) return;
+    setInput(input);
+  };
+
+  const onSubmit = (e: FormEvent) => {
     e.preventDefault();
     if (input === "") return;
-    createBubble(input);
+    addBubble(input);
     setInput("");
   };
+
   return (
-    <div className="fixed bottom-2">
-      <form onSubmit={onSubmit}>
-        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-        <input type="submit" value="Add" />
-      </form>
-    </div>
+    <form onSubmit={onSubmit} className="fixed bottom-4 flex gap-2 px-4 py-2 rounded-2xl bg-slate-400">
+      <input type="text" value={input} onChange={handleInput} />
+      <input type="submit" value="Add" />
+    </form>
   );
 }
