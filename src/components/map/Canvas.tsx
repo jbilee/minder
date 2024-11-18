30f75cf86d5173ee8746539f4d86f3d2a71e402a
jbilee <julishahn@gmail.com>
2024-11-18 12:50:45 +0900

    refactor: Further refine push-away effect and rename RectConfig

--- src/components/map/Canvas.tsx
+++ src/components/map/Canvas.tsx
@@ -17,14 +17,14 @@ type BubbleProps = {
   childNodes: string[];
 };
 
-type RectConfig = {
+type RectDimensions = {
   x: number;
   y: number;
   width: number;
   height: number;
 };
 
-const haveIntersection = (r1: RectConfig, r2: RectConfig) => {
+const haveIntersection = (r1: RectDimensions, r2: RectDimensions) => {
   return !(r2.x > r1.x + r1.width || r2.x + r2.width < r1.x || r2.y > r1.y + r1.height || r2.y + r2.height < r1.y);
 };
 
@@ -40,13 +40,13 @@ const deactivate = (target: Konva.Group) => {
   rect.stroke("");
 };
 
-const getCoverage = (r1: RectConfig, r2: RectConfig) => {
+const getCoverage = (r1: RectDimensions, r2: RectDimensions) => {
   const xDifference = r1.x < r2.x ? r1.x + r1.width - r2.x : r2.x + r2.width - r1.x;
   const yDifference = r1.y < r2.y ? r1.y + r1.height - r2.y : r2.y + r2.height - r1.y;
   return xDifference * yDifference;
 };
 
-const getClosestCorner = (target: RectConfig, dragged: RectConfig) => {
+const getClosestCorner = (target: RectDimensions, dragged: RectDimensions) => {
   const xAxis = target.x - dragged.x;
   const yAxis = target.y - dragged.y;
   const xCorner = xAxis <= 0 ? 1 : 0;
@@ -54,7 +54,7 @@ const getClosestCorner = (target: RectConfig, dragged: RectConfig) => {
   return { x: xCorner, y: yCorner };
 };
 
-const findClosest = (array: Konva.Group[], target: RectConfig) => {
+const findClosest = (array: Konva.Group[], target: RectDimensions) => {
   const closest = array.reduce((max, cur) => {
     const maxCoverage = max ? getCoverage(max.getClientRect(), target) : 0;
     const curCoverage = getCoverage(cur.getClientRect(), target);
@@ -63,7 +63,7 @@ const findClosest = (array: Konva.Group[], target: RectConfig) => {
   return closest;
 };
 
-const getObjectsInRange = (array: Konva.Group[], target: RectConfig) => {
+const getObjectsInRange = (array: Konva.Group[], target: RectDimensions) => {
   const inRange = array.reduce<Konva.Group[]>((arr, cur) => {
     const coverage = getCoverage(cur.getClientRect(), target);
     if (coverage > 0) arr.push(cur);
@@ -73,16 +73,22 @@ const getObjectsInRange = (array: Konva.Group[], target: RectConfig) => {
   return inRange;
 };
 
-const pushObjectAway = (target: Konva.Shape, draggedTarget: Konva.Group, callback?: (t: Konva.Shape) => void) => {
-  const targetConfig = target.getClientRect();
-  const draggedConfig = draggedTarget.getClientRect();
+const pushObjectAway = (dragTarget: Konva.Shape, dropTarget: Konva.Group) => {
+  const dragTargetDims = dragTarget.getClientRect();
+  const dropTargetDims = dropTarget.getClientRect();
+  const xDiff = Math.abs(dragTargetDims.x - dropTargetDims.x);
+  const moveX = xDiff >= 160;
 
-  const yMove =
-    targetConfig.y <= draggedConfig.y
-      ? targetConfig.y + targetConfig.height - draggedConfig.y
-      : -(draggedConfig.y + draggedConfig.height - targetConfig.y);
-
-  draggedTarget.move({ x: 0, y: yMove });
+  if (moveX) {
+    if (dragTargetDims.x <= dropTargetDims.x) {
+      return dropTarget.move({ x: dragTargetDims.x + dragTargetDims.width - dropTargetDims.x, y: 0 });
+    }
+    return dropTarget.move({ x: -(dropTargetDims.x + dropTargetDims.width - dragTargetDims.x), y: 0 });
+  }
+  if (dragTargetDims.y <= dropTargetDims.y) {
+    return dropTarget.move({ x: 0, y: dragTargetDims.y + dragTargetDims.height - dropTargetDims.y });
+  }
+  dropTarget.move({ x: 0, y: -(dropTargetDims.y + dropTargetDims.height - dragTargetDims.y) });
 };
 
 export default function Canvas() {
