99c727ac342cb466f96da1ac9cd7d76b4c2ce390
jbilee <julishahn@gmail.com>
2024-11-24 23:17:48 +0900

    feat: Map fetching from Supabase DB

--- src/app/(home)/maps/page.tsx
+++ src/app/(home)/maps/page.tsx
@@ -1,4 +1,18 @@
-export default function Maps() {
-  // fetch maps
-  return <main className="flex min-h-screen flex-col items-center justify-center"></main>;
+import MapCard from "@/components/map/MapCard";
+import { createSupabaseServerClient } from "@/utils/supabase/server";
+
+export default async function Maps() {
+  const supabase = await createSupabaseServerClient();
+  const { data, error } = await supabase.from("maps").select("id, name, created_at, updated_at, thumbnail");
+  if (error) {
+    // TODO: Handle exception
+  }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      {data?.length &&
+        data.map(({ id, name, created_at, updated_at, thumbnail }) => (
+          <MapCard key={id} mapId={id} mapName={name} imgUrl={thumbnail} lastEdited={updated_at ?? created_at} />
+        ))}
+    </main>
+  );
 }
