e9df16ab890bcd4297ccbeb6ea1ea9eb7e4c1719
jbilee <julishahn@gmail.com>
2024-12-02 23:03:01 +0900

    feat: Complete welcome page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { redirect } from "next/navigation";
+import Image from "next/image";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function HomePage() {
@@ -9,5 +10,29 @@ export default async function HomePage() {
     // Log
   }
   if (user) redirect("/main");
-  return <div className="flex flex-col max-w-[100dvw] lg:max-w-4xl px-4 lg:px-0 gap-6">Welcome!</div>;
+  return (
+    <div className="flex flex-col max-w-[100dvw] lg:max-w-6xl px-4 lg:px-0 lg:pt-14 gap-6">
+      <div className="py-2 text-4xl text-center lg:text-left lg:text-6xl font-bold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+        Visualize your mind
+      </div>
+      <p className="text-center">
+        Minder is a simple, easy-to-use mind map app meant to help you keep track of fleeting thoughts.
+      </p>
+      <p className="text-center">Organize what&apos;s in your head into connected thought bubbles.</p>
+      <Image
+        src="https://utfs.io/f/5Jr9cM2jiS1IDB9vq4fbLaNeviYoIrHfXEWTGMn9jdmFZ3q1"
+        width={527}
+        height={333}
+        alt="Mind map sample"
+        className="lg:hidden self-center"
+      />
+      <Image
+        src="https://utfs.io/f/5Jr9cM2jiS1IljLCzYkoFgcmbpRfW8wt20e4k9diDyxASa6M"
+        width={702}
+        height={374}
+        alt="Mind map sample"
+        className="hidden lg:block self-center"
+      />
+    </div>
+  );
 }
