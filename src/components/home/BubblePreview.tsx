8ec880b4cb18483416b1f100c0c8817b2596c7cd
jbilee <julishahn@gmail.com>
2024-12-03 23:01:50 +0900

    refactor: Improve bubble preview UI
    - Increased width on desktop screens

--- src/components/home/BubblePreview.tsx
+++ src/components/home/BubblePreview.tsx
@@ -10,7 +10,7 @@ type BubblePreviewProps = {
 
 export default function BubblePreview({ text, createdAt, mapId, mapName }: BubblePreviewProps) {
   return (
-    <div className="p-2 rounded-md border bg-white dark:bg-slate-800 border-neutral-300 dark:border-slate-600">
+    <div className="lg:w-80 p-2 rounded-md border bg-white dark:bg-slate-800 border-neutral-300 dark:border-slate-600">
       <p className="mb-2 truncate">{text}</p>
       <p className="text-right text-sm text-slate-700 dark:text-slate-400">
         {extractDate(createdAt)} {extractTime(createdAt)}
