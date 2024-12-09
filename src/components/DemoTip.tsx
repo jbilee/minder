d8ab481461616ca330e1004e8018d1d41a0c4fb4
jbilee <julishahn@gmail.com>
2024-12-09 11:14:39 +0900

    refactor: Improve demo tip component UI

--- src/components/DemoTip.tsx
+++ src/components/DemoTip.tsx
@@ -1,9 +1,9 @@
 export default function DemoTip() {
   return (
-    <div className="fixed bottom-4 left-4 px-4 py-2 rounded-md text-slate-50 bg-sky-700">
+    <div className="fixed bottom-4 left-4 mr-4 px-4 py-2 text-sm rounded-md text-slate-50 bg-sky-700">
       <p className="mb-2">Feel free to use our demo account to log in!</p>
-      <p>Email: demo@mindermap.com</p>
-      <p>Password: mindermap</p>
+      <p className="font-light">Email: demo@mindermap.com</p>
+      <p className="font-light">Password: mindermap</p>
     </div>
   );
 }
